e9d7b5b5460e050c1b55ec3fea99590104f865a6
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2017-02-07 15:32:09 +0100

    Remove unneeded cowboy dependency

--- mix.exs
+++ mix.exs
@@ -33,7 +33,6 @@ defmodule CorsPlug.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:cowboy, "~> 1.0.0"},
       {:plug, "> 0.8.0"},
 
       {:ex_doc, "~> 0.11", only: :dev},
